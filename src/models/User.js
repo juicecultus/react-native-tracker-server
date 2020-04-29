4d3239a7a381a07f798b5487be5d648f29aa233f
juicecultus <juicecultus@gmail.com>
2020-04-29 23:08:54 +0100

    protecting signin route

--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', function (next) {
   const user = this;
+
   if (!user.isModified('password')) {
     return next();
   }
@@ -37,6 +38,7 @@ userSchema.pre('save', function (next) {
 
 userSchema.methods.comparePassword = function (candidatePassword) {
   const user = this;
+
   return new Promise((resolve, reject) => {
     bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
       if (err) {
